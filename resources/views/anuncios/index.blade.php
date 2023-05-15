d37f91b406cc5a7e8c335d9393a24e3601c64d1b
Jesús Quintana <jesquiliche@hotmail.comm>
2023-05-15 23:25:21 +0200

    Limpiando código

--- resources/views/anuncios/index.blade.php
+++ resources/views/anuncios/index.blade.php
@@ -2,121 +2,93 @@
 
 @section('title', 'Página de inicio')
 
-@section('estilos')
-    <style>
-        .carousel-control-prev-icon,
-        .carousel-control-next-icon {
-
-
-            color: black;
-        }
-    </style>
-@endsection
-
-
-
-
 @section('content')
-    
     <div class="container-fluid col-lg-12 mt-8">
-    
-        
         <div class="row">
-
             <div class="card2 col-lg-3 mx-auto">
-                <h4 class="text-center  mx-auto resaltado"><b>Filtro</b></h4>
-                    <div class="container mt-4">
-                        <form action="{{route('home.filterMultiple')}}" method="POST">
-                            @csrf
-                                <div class="col-md-12">
-                                    <label for="desde" class="form-label">
-                                        <p class="text-sm">Título<p>
-                                    </label>
-                                    <div class="mb-3">
+                <h4 class="text-center  mx-auto resaltado mt-3"><b>Filtro</b></h4>
+                <div class="container mt-4">
+                    <form action="{{ route('home.filterMultiple') }}" method="POST">
+                        @csrf
+                        <div class="col-md-12">
+                            <label for="desde" class="form-label">
+                                <p class="text-sm">Título
+                                <p>
+                            </label>
+                            <div class="mb-3">
 
-                                        <input type="text" placeholder="Título" class="form-control" id="titulo"
-                                            name="titulo" value="{{$titulo}}"">
-                                    </div>
-                                </div>
-                                <div class="col-md-12">
-                                    <div class="mb-3">
-                                        <label for="desde" class="form-label">
-                                            <p class="text-sm">Población<p>
-                                        </label>
-                                        <input type="text" placeholder="Población" class="form-control" id="poblacion"
-                                            value="{{$poblacion}}" name="poblacion">
-                                    </div>
-                                </div>
-                            
-                        
-                            <div class="row col-lg-12 mx-auto">
-                                <div class="col-md-6">
-                                    <label for="desde" class="form-label">
-                                        <p class="text-sm">Desde..
-                                        <p>
-                                    </label>
-                                    <input type="number" class="form-control" id="desde" name="desde" min="0"
-                                        value=0
-                                        max="10000">
-                                </div>
-                                <div class="col-md-6">
-                                    <label for="hasta" class="form-label">
-                                        <p class="text-sm">Hasta
-                                        <p>
-                                    </label>
-                                    <input type="number" class="form-control" id="hasta" name="hasta" min="0"
-                                        value="10000"
-                                        max="10000">
-                                </div>
+                                <input type="text" placeholder="Título" class="form-control" id="titulo"
+                                    name="titulo" value="{{ $titulo }}"">
                             </div>
-                            
-
-                            <div class="col-lg-12">
-                                
-                                <div class="mb-2">
-                                    <label>
-                                        <p class="text-sm">Categoría<p>
-                                    </label>
-                                    <select name="subcategoria_id" class="form-control">
-                                    
-                                        @foreach ($categorias as $categoria)
-                                            <optgroup label="{{ $categoria->nombre }}">
-                                               
-                                                @foreach ($categoria->subcategorias as $subcategoria)
-                                                <option value="{{ $subcategoria->id }}" {{ $subcategoria->id == $subcategoriaId ? 'selected' : '' }}>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="mb-3">
+                                <label for="desde" class="form-label">
+                                    <p class="text-sm">Población
+                                    <p>
+                                </label>
+                                <input type="text" placeholder="Población" class="form-control" id="poblacion"
+                                    value="{{ $poblacion }}" name="poblacion">
+                            </div>
+                        </div>
+                        <div class="row col-lg-12 mx-auto">
+                            <div class="col-md-6">
+                                <label for="desde" class="form-label">
+                                    <p class="text-sm">Desde..
+                                    <p>
+                                </label>
+                                <input type="number" class="form-control" id="desde" name="desde" min="0"
+                                    value=0 max="10000">
+                            </div>
+                            <div class="col-md-6">
+                                <label for="hasta" class="form-label">
+                                    <p class="text-sm">Hasta
+                                    <p>
+                                </label>
+                                <input type="number" class="form-control" id="hasta" name="hasta" min="0"
+                                    value="10000" max="10000">
+                            </div>
+                        </div>
+                        <div class="col-lg-12">
+                            <div class="mb-2">
+                                <label>
+                                    <p class="text-sm">Categoría
+                                    <p>
+                                </label>
+                                <select name="subcategoria_id" class="form-control">
+                                    @foreach ($categorias as $categoria)
+                                        <optgroup label="{{ $categoria->nombre }}">
+                                            @foreach ($categoria->subcategorias as $subcategoria)
+                                                <option value="{{ $subcategoria->id }}"
+                                                    {{ $subcategoria->id == $subcategoriaId ? 'selected' : '' }}>
                                                     {{ $subcategoria->nombre }}
                                                 </option>
-                                                
-                                                @endforeach
-                                            </optgroup>
-                                        @endforeach
-                                    </select>
-
-                                </div>
-                              
+                                            @endforeach
+                                        </optgroup>
+                                    @endforeach
+                                </select>
                             </div>
-                            <div class="col-lg-12">
-                                <div class="mb-2">
-                                    <label>
-                                        <p class="text-sm">Estado<p>
-                                    </label>
-
-                                    <select name="estado_id" class="form-control">
-
-                                        @foreach ($estados as $estado)
-                                            <option value="{{ $estado->id }}">{{ $estado->nombre }}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-
+                        </div>
+                        <div class="col-lg-12">
+                            <div class="mb-2">
+                                <label>
+                                    <p class="text-sm">Estado
+                                    <p>
+                                </label>
+                                <select name="estado_id" class="form-control">
+                                    @foreach ($estados as $estado)
+                                        <option value="{{ $estado->id }}">{{ $estado->nombre }}</option>
+                                    @endforeach
+                                </select>
                             </div>
-                            <button type="submit" class="btn btn-danger col-lg-12 m-1">buscar</button>
-                        </form>
-                    </div>
 
+                        </div>
+                        <button type="submit" class="btn btn-danger col-lg-12 m-1">buscar</button>
+                    </form>
+                </div>
             </div>
             <div class="card3 col-lg-8 mx-auto ">
-                <h4 class="text-center resaltado"><b>Anuncios</b></h4>
+                <h4 class="text-center resaltado mx-auto mt-3"><b>Anuncios</b></h4>
                 <div class="row ">
                     @foreach ($anuncios as $anuncio)
                         <div class="card3 col-lg-4 mx-auto my-1">
@@ -127,10 +99,6 @@
                                 <div class="card-body m-1">
                                     precio : <b>{{ $anuncio->precio }}</b>
                                     <img src="{{ $anuncio->imagen }}" class="d-block w-100" alt="{{ $anuncio->titulo }}">
-
-
-
-
                                 </div>
                             </a>
                         </div>
--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -5,15 +5,19 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Test TAI</title>
+    <title>Anuncios segunda mano</title>
     <!-- Otros elementos del head -->
+
+    <!-- Incluye los estilos de Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css"
         integrity="sha512-rqQltXRuHxtPWhktpAZxLHUVJ3Eombn3hvk9PHjV/N5DMUYnzKPC1i3ub0mEXgFzsaZNeJcoE0YHq0j/GFsdGg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
 
+    <!-- Incluye los estilos de Bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
+    <!-- Incluye los scripts de jQuery, Popper.js y Bootstrap -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
         integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
     </script>
@@ -23,85 +27,93 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
     </script>
+
+    <!-- Incluye los estilos de Font Awesome (versión 5.15.3) -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 
     <title>@yield('title')</title>
-    
-    
+
+    <!-- Incluye los estilos personalizados -->
     <link rel="stylesheet" href="{{ asset('css/anuncios.css') }}">
+
     @yield('estilos')
+    <!-- Incluye estilos específicos de la vista actual -->
 </head>
 <main>
-
+    <!-- Barra de navegación -->
     <div class="menu container-fluid fixed-top bg-white">
         <div class="container-fluid">
-        <nav class="navbar navbar-expand-lg navbar-light bg-white ">
-            <a class="navbar-brand" href="/">
-                <img src="/images/logo.png" width="60" alt="..." loading="lazy">
-                <span class="nav-item resaltado">Anuncios segunda mano</span></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+            <nav class="navbar navbar-expand-lg navbar-light bg-white">
+                <a class="navbar-brand" href="/">
+                    <img src="/images/logo.png" width="60" alt="..." loading="lazy">
+                    <span class="nav-item resaltado">Anuncios segunda mano</span></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active">
-                        <a class="nav-item" href="{{ route('home') }}"><i class="fas fa-home"></i> Home<span
-                                class="sr-only">(current)</span></a>
-                    </li>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a class="nav-item" href="{{ route('home') }}"><i class="fas fa-home"></i> Home<span
+                                    class="sr-only">(current)</span></a>
+                        </li>
 
-                    <li class="nav-item ml-2 dropdown">
-                        <a class="nav-item dropdown-toggle ml-2" href="#" id="navbarDropdown" role="button"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="fas fa-user-plus"></i>
-                            @if (!auth()->check())
-                            @else
-                                {{ auth()->user()->name }}
-                            @endif
+                        <li class="nav-item ml-2 dropdown">
+                            <a class="nav-item dropdown-toggle ml-2" href="#" id="navbarDropdown" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="fas fa-user-plus"></i>
+                                @if (!auth()->check())
+                                @else
+                                    {{ auth()->user()->name }}
+                                @endif
 
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('registro') }}">Registro</a>
-                            @if (!auth()->check())
-                                <a class="dropdown-item" href="{{ route('login2') }}">Iniciar sesión</a>
-                            @endif
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('registro') }}">Registro</a>
+                                @if (!auth()->check())
+                                    <a class="dropdown-item" href="{{ route('login2') }}">Iniciar sesión</a>
+                                @endif
 
-                            @can('admin.home')
-                                <a class="dropdown-item" href={{ route('admin.home') }}>Administración</a>
-                            @endcan
-                            <div class="dropdown-divider"></div>
-                            @if (auth()->check())
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                    style="display: none;">
-                                    @csrf
-                                </form>
+                                @can('admin.home')
+                                    <a class="dropdown-item" href={{ route('admin.home') }}>Administración</a>
+                                @endcan
+                                <div class="dropdown-divider"></div>
+                                @if (auth()->check())
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                        style="display: none;">
+                                        @csrf
+                                    </form>
 
-                                <a class="dropdown-item " href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
-                                    document.getElementById('logout-form').submit();">
-                                    Logout
-                                </a>
-                            @endif
+                                    <a class="dropdown-item " href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                                        Logout
+                                    </a>
+                                @endif
 
-                        </div>
-                    </li>
+                            </div>
+                        </li>
 
 
-                    <li class="nav-item ml-2">
-                        <a href="{{route('anuncios.create')}}"><i class="far fa-file-alt"></i> Publicar anuncio</a>
-                    </li>
+                        <li class="nav-item ml-2">
+                            <a href="{{ route('anuncios.create') }}"><i class="far fa-file-alt"></i> Publicar
+                                anuncio</a>
+                        </li>
 
-                </ul>
+                    </ul>
 
-            </div>
-        </nav>
+                </div>
+            </nav>
         </div>
 
     </div>
 
     @yield('content')
+    <!-- Contenido específico de cada vista -->
 
+    <!-- Footer -->
     <footer>
         <div class="container-fluid text-center mt-5 bg-dark">
             <h5 class="text-gray-300">Copyrigth Jesús Quintana Esquiliche</h5>
@@ -112,8 +124,6 @@
             </ul>
         </div>
     </footer>
-
 </main>
-
 
 </html>
