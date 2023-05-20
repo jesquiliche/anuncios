c31a7f8c5ce76d9bed8ef09fb864aaae8232d14c
Jesús Quintana <jesquiliche@hotmail.comm>
2023-05-20 21:10:40 +0200

    Traducido al español

--- lang/es/auth.php
+++ lang/es/auth.php
@@ -1,23 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
-    |
-    */
-
-    'failed' => 'Estas credenciales no coinciden con nuestros registros.',
-    'password' => 'La contraseña proporcionada es incorrecta.',
-    'throttle' => 'Demasiados intentos de inicio de sesión. Por favor, inténtelo de nuevo en :seconds segundos.',
-    'email' => 'Correo electrónico',
-    'password' => 'Contraseña',
-    'login' => 'Iniciar sesión',
-
+    'email'    => 'Correo electrónico',
+    'failed'   => 'Estas credenciales no coinciden con nuestros registros.',
+    'login'    => 'Iniciar sesión',
+    'password' => 'La contraseña es incorrecta.',
+    'throttle' => 'Demasiados intentos de acceso. Por favor intente nuevamente en :seconds segundos.',
 ];
