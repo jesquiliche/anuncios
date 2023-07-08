306c46bac0bb7e7a711311f6d45c91d1484cf95c
Jesús Quintana <jesquiliche@hotmail.comm>
2023-07-08 06:04:01 +0200

    Praparando para producción

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -31,6 +31,8 @@ class RouteServiceProvider extends ServiceProvider
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
 
+
+        
         $this->routes(function () {
             Route::middleware('api')
                 ->prefix('api')
